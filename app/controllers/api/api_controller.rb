8a4d735bb54c6e4ae0d20484c89add19d841d2a0
Sikandar Saleem <sikandarsaleem2000@gmail.com>
2023-08-14 13:00:50 +0500

    Add: API Controller

---
+++ app/controllers/api/api_controller.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+module Api
+  # ApiContoller
+  class ApiController < ApplicationController
+    def render_success_response(json_response,  response_code = :ok)
+      render json: json_response.merge(success: true), status: response_code
+    end
+
+    def render_error_response(json_response, response_code = :unprocessable_entity, errors = nil)
+      if errors.present?
+        errors = errors.full_messages if errors.is_a?(ActiveModel::Errors)
+        json_response = json_response.merge(errors:)
+      end
+
+      render json: json_response.merge(success: false), status: response_code
+    end
+
+    def render_not_found(errors = ['Requested resource not found.'])
+      errors = [errors] if errors.is_a? String
+      render_error_response({}, :not_found, errors)
+    end
+  end
+end
